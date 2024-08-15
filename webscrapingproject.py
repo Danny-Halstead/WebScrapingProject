f2fc5e913f969469d62dddfd75ae93e114ac04c1
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-15 17:35:14 -0500

    Imported and setup BeautifulSoup and Requests libraries

--- webscrapingproject.py
+++ webscrapingproject.py
@@ -1,1 +1,13 @@
-print("yeet")
+from bs4 import BeautifulSoup
+import requests
+
+print('Testing')
+'''
+with open('Simple.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+article = soup.find('div', class_='article')
+print(article)
+
+headline = 
+'''
