3693952ea68116f6e926f4a7f458263c15c7b641
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-15 23:06:12 -0500

    Outputs the webscraped headlines, summaries, amd YT link to a csv file.

--- webscrapingproject.py
+++ webscrapingproject.py
@@ -1,9 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('https://web.archive.org/web/20171019235429/https://coreyms.com/').text #might not work properly due to being a web archive version of the site
 
 soup = BeautifulSoup(source, 'lxml')
+
+csv_file = open('cms_scrape.csv', 'w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline', 'summary', 'video_link'])
+
 
 for article in soup.find_all('article'):
     headline = article.h2.a.text
@@ -12,12 +19,20 @@ for article in soup.find_all('article'):
     summary = article.find('div', class_='entry-content').p.text
     print(summary)
 
-    vid_src = article.find('iframe', class_='youtube-player')['src']
+    try:
+        vid_src = article.find('iframe', class_='youtube-player')['src']
 
-    vid_id = vid_src.split('/')[9]
-    vid_id = vid_id.split('?')[0]
+        vid_id = vid_src.split('/')[9]
+        vid_id = vid_id.split('?')[0]
 
-    yt_link = f'https://youtube.com/watch/?v={vid_id}'
+        yt_link = f'https://youtube.com/watch/?v={vid_id}'
+    except Exception as e:
+        yt_link = None
+
     print(yt_link)
 
     print()
+
+    csv_writer.writerow([headline, summary, yt_link])
+
+csv_file.close()
