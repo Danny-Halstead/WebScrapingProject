d3099a097b722e349f3ef2c129472bac856ee71e
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-15 22:13:41 -0500

    Article summaries

--- webscrapingproject.py
+++ webscrapingproject.py
@@ -1,13 +1,8 @@
 from bs4 import BeautifulSoup
 import requests
 
+source = requests
+
 with open('Simple.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-for article in soup.find_all('div', class_='article'):
-
-    headline = article.h2.a.text
-    print(headline)
-
-    summary = article.p.text
-    print(summary)
