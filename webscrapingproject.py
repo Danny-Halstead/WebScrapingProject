77c388ddcc280aa4d38ca8ebb947a0e8683cc994
Danny-Halstead <dannyrhalstead@gmail.com>
2024-08-15 21:12:41 -0500

    Downloaded modules

--- webscrapingproject.py
+++ webscrapingproject.py
@@ -1,13 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-print('Testing ok')
-'''
 with open('Simple.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
-article = soup.find('div', class_='article')
-print(article)
-
-headline = 
-'''
+print(soup)
